d8490d6524b2c8ad0323bcb097d1cfbd85ccd733
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-17 10:04:34 -0700

    work sheet has been updated

--- work_sheet.py
+++ work_sheet.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 17 09:54:37 2018
+Created on Sat Mar 17 09:57:41 2018
 
 @author: bijayamanandhar
 """
-
-x = ['a', 'b', 'c', 'd']
-for i, x in enumerate(x):
-    print(i, x)
-
-
-for i in range(len(x)):
-        print(i, x, 9)
-
-basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
-for fruit in sorted(set(basket)):
-    print(fruit )
 
