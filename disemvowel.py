d61f1001df73fc593e444f47c9952c13d2b58712
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-24 14:57:17 -0700

    this code removes all vowels from a string

--- disemvowel.py
+++ disemvowel.py
@@ -6,6 +6,8 @@ Created on Sat Mar 24 14:54:03 2018
 @author: bijayamanandhar
 """
 
+"""while loop"""
+
 def disemvowel(text):
     x = ''
     i = 0
@@ -17,3 +19,13 @@ def disemvowel(text):
         i += 1
     return x
 print(disemvowel('America'))
+
+""" for loop"""
+
+def disemvowel(text):
+    x = ''
+    for i in text:
+        if i not in 'aAeEiIoOuU':
+            x += i
+    return x
+print(disemvowel('United States of America'))
