9116c0d254587aa319a476904f6f774fbb6afab9
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-25 19:38:57 -0700

    some examples for this code

--- longest_bigram.py
+++ longest_bigram.py
@@ -11,7 +11,7 @@ def longest_bigram(str):
     str_split = str.split()
     longest_bigram = ''
     
-    for i in range(len(str_split)-1):
+    for i in range(len(str_split) - 1):
         
         x = str_split[i] + ' ' + str_split[i+1]
         
@@ -20,5 +20,8 @@ def longest_bigram(str):
             longest_bigram = x
     
     return longest_bigram
-print(longest_bigram("One must have a mind of winter") == 'must have')
-
+print(longest_bigram("One must have a mind of winter") == "must have")
+print(longest_bigram("Where there is a will there is a way") == "Where there")
+print(longest_bigram("Sky is the limit") == "the limit")
+print(longest_bigram("Make hay while sun shines") == "sun shines")
+print(longest_bigram("A stitch in time saves nine") == "time saves")
