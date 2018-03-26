905229033ba8efdd18697d4f629edcb740bb2b0b
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-26 14:10:02 -0700

    Asimpler function has been made

--- remove_nth_letter.py
+++ remove_nth_letter.py
@@ -8,12 +8,20 @@ Created on Sun Mar 25 13:54:14 2018
 
 def remove_nth_letter(string, n):
     
-    x = list(string)
     new_str = ''
     
-    for i in range(len(x)):
+    for i in range(len(string)):
         if i != n:
-            new_str += x[i]
+            new_str += string[i]
     return new_str
 
 print(remove_nth_letter('abcXdef', 3) == 'abcdef')
+
+
+#Function 2:
+
+def remove_nth_letter(string, n):
+    
+    return string[:n]+string[n+1:]
+
+print(remove_nth_letter('abcXdef', 3))
