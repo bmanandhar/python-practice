22480dd268b0528c92e9c7c101893a3bc4837984
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-26 18:54:18 -0700

    isogram_matcher being corrected

--- isogram_matcher.py
+++ isogram_matcher.py
@@ -15,23 +15,25 @@ Created on Sat Mar 24 15:13:25 2018
 # and the second is the number of letters matched in both words but not in the
 # same position.
 
-def isogram_matcher(wordA, wordB):
-    ind1, ind2 = 0, 0
+def isogram_matcher(word1, word2):
+    #x-> number of matching letters at the same positions
+    #y-> numbers of matching letters at different positions
+    x, y = 0, 0
 
-    for i in range(len(wordA)):
-        if wordA[i] == wordB[i]:
-            ind1 += 1
+    for i in range(len(word1)):
+        if word1[i] == word2[i]:
+            x += 1
 
-        for j in range(len(wordB)):
-            if wordA[i] == wordB[j]\
+        for j in range(len(word1)):
+            if word1[i] == word2[j]\
             and i != j:
-                ind2 += 1
+                y += 1
 
-    return [ind1, ind2]    
+    return [x, y]
 print(isogram_matcher("ultrasonic", "ostracized"))
-            
+
 '''
-#print(isogram_matcher("ultrasonic", "ostracized"))               
+#print(isogram_matcher("ultrasonic", "ostracized"))
 # print isogram_matcher("an", "at") == [1, 0]
 print isogram_matcher("cat", "car") == [2, 0]
 print isogram_matcher("cat", "tap") == [1,1]
