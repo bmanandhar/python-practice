94e6c95a5fdf3100e13982b3a547a84dd0bdc0d8
bmanandhar <bijaya.manandhar@hotmail.com>
2018-03-27 10:40:50 -0700

    programmed shortened

--- high_card_points.py
+++ high_card_points.py
@@ -6,24 +6,22 @@ Created on Sun Mar 25 13:52:06 2018
 @author: bijayamanandhar
 """
 
-def high_card_points(hands):
+def high_card_points(hand):
 
     points = 0
-    for i in range(len(hands)):
+    for i in hand:
 
-        if hands[i] == "J":
+        if i == "J":
             points += 1
 
-        elif hands[i] == "Q":
+        elif i == "Q":
             points += 2
 
-        elif hands[i] == "K":
+        elif i == "K":
             points += 3
 
-        elif hands[i] == "A":
+        elif i == "A":
             points += 4
 
     return points
 print(high_card_points(["2","3","4","5","6","7","8","9","10","J","Q","K","A"]))
-
-
