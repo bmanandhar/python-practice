320e6f4d1d193c3d49f779d2d08bc6460d8595fe
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-01 06:32:19 -0700

    no_repeats function using helper method being added

--- 02_no_repeats.py
+++ 02_no_repeats.py
@@ -8,7 +8,33 @@ Created on Sat Mar 17 09:27:34 2018
 
 # 02 No Repeats Years
 
+# no_repeats using a helper function :
 
+def dup(num_list):
+    
+    counter = 0
+    chk_list = []
+    for i in num_list:
+        if i not in chk_list:
+            chk_list.append(i)
+        else:
+            counter += 1
+    return len(chk_list) == len(num_list)
+        
+def no_repeats(start, end):
+    no_repeats = []
+    
+    for i in range(start, end + 1):
+        year_list = list(str(i))
+        if dup(year_list):
+            no_repeats.append(i)
+    return no_repeats
+
+print(no_repeats(1987, 2018))
+
+#----------------------------------
+
+#no_repeats using set and list
 def no_repeats(year_start, year_end):
 
     years = []
