bb2a8deb51106c83588a4274b9b484349871c12b
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-01 06:55:06 -0700

    hk_tel.py for checing tel-no format

--- hk_tel.py
+++ hk_tel.py
@@ -9,8 +9,8 @@ Created on Sat Mar 31 07:02:39 2018
 def hk_tel(n): 
     if n[:4].isdigit() and n[5:10].isdigit() and n[4] == " " and len(n) == 9:
         return True
-    else:
-        return False
+    
+    return False
 print(hk_tel('1234 5678') == True)
 print(hk_tel('1234 5#78') == False)
 print(hk_tel('365416578') == False)             
