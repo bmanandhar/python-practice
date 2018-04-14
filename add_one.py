39a3791f8c635a6781ccedaa709d9a671c8300ed
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-14 15:30:25 -0700

    odometer of a car

--- add_one.py
+++ add_one.py
@@ -16,13 +16,15 @@ the results as follows:
 
 def add_one(array):
     carry = 1
+    #iterates from the end of array
     for i in range(len(array) -1, -1, -1):
         total = array[i] + carry
         if total % 10 == 0:
             array[i] = 0
         else:
             carry = 0 
-            array[i] = total % 10 
+            array[i] = total % 10
+    #check if carry is till 1 after final iteration
     if carry == 1:
         array = [1] + array                  
     return array
@@ -31,5 +33,3 @@ print(add_one([2,5,9]) == [2,6,0])
 print(add_one([3,9,9]) == [4,0,0])
 print(add_one([9,1,9,9]) == [9,2,0,0])
 print(add_one([9,9,9,9]) == [1,0,0,0,0])
-
-
