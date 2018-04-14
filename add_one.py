9fd02ae5135046ac331bc62c38de6d8c8972a149
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-14 15:08:49 -0700

    odeometer of a car

---
+++ add_one.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+'''
+Write a python code that will return 
+the results as follows:
+[1,2,3] will return [1,2,4],
+[2,5,9] will return [2,6,0],
+[3,9,9] will return [4,0,0],
+[9,1,9,9] will return [9,2,0,0] and
+[9,9,9,9] will return [1,0,0,0,0] 
+'''
+
+def add_one(array):
+    carry = 1
+    for i in range(len(array) -1, -1, -1):
+        total = array[i] + carry
+        if total % 10 == 0:
+            array[i] = 0
+        else:
+            carry = 0 
+            array[i] = total % 10 
+    if carry == 1:
+        array = [1] + array                  
+    return array
+print(add_one([1,2,3]) == [1,2,4])
+print(add_one([2,5,9]) == [2,6,0])
+print(add_one([3,9,9]) == [4,0,0])
+print(add_one([9,1,9,9]) == [9,2,0,0])
+print(add_one([9,9,9,9]) == [1,0,0,0,0])
+
+
