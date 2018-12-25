fc8b15e616c4e5978655c50fc616dd430aecc5f2
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-12-24 17:25:51 -0800

    commit all

--- add_one.py
+++ add_one.py
@@ -10,7 +10,7 @@ Write a python program returns as follows:
 [2,5,9] --> [2,6,0],
 [3,9,9] --> [4,0,0],
 [9,1,9,9] --> [9,2,0,0] and
-[9,9,9,9] --> [1,0,0,0,0] 
+[9,9,9,9] --> [1,0,0,0,0]
 '''
 
 def add_one(array):
@@ -21,11 +21,11 @@ def add_one(array):
         if total % 10 == 0:
             array[i] = 0
         else:
-            carry = 0 
+            carry = 0
             array[i] = total % 10
     #checks if carry is still 1 after final iteration
     if carry == 1:
-        array = [1] + array                  
+        array = [1] + array
     return array
 
 print(add_one([1,2,3]) == [1,2,4])
